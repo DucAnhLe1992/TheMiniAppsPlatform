e206629472c4ceb116e5d9475bc07c139cee0462
Duc Anh Le <gerald.duc.anh.le@gmail.com>
2025-11-20 11:41:56 +0200

    Updated vite.config.ts

--- apps/shell/vite.config.ts
+++ apps/shell/vite.config.ts
@@ -1,20 +1,23 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from 'path'
+import { fileURLToPath } from 'url'
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url))
 
 export default defineConfig({
   plugins: [react()],
-  root: '.',
-  envDir: '../../',
-  publicDir: 'public',
+  root: __dirname,
+  envDir: path.join(__dirname, '../../'),
+  publicDir: path.join(__dirname, 'public'),
   build: {
-    outDir: '../../dist/apps/shell',
+    outDir: path.join(__dirname, '../../dist/apps/shell'),
     emptyOutDir: true
   },
   resolve: {
     alias: {
-      '@': path.resolve(process.cwd(), 'src'),
-      '@shared': path.resolve(process.cwd(), '../../libs/shared/src')
+      '@': path.join(__dirname, 'src'),
+      '@shared': path.join(__dirname, '../../libs/shared/src')
     }
   },
   server: {
