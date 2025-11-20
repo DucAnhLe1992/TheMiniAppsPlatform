c3b3ecc8ec843997bf8cd6170d23e6cf0e85c934
Duc Anh Le <gerald.duc.anh.le@gmail.com>
2025-11-20 03:01:07 +0200

    Updated vite.config.ts

--- apps/shell/vite.config.ts
+++ apps/shell/vite.config.ts
@@ -5,6 +5,7 @@ import path from 'path'
 export default defineConfig({
   plugins: [react()],
   root: __dirname,
+  envDir: path.resolve(__dirname, '../../'),
   build: {
     outDir: '../../dist/apps/shell',
     emptyOutDir: true
